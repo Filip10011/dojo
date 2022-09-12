3ac6044ae611b73036f3a5c7aedc49c242b867b5
Filip Popovski <popovski_f@hotmail.com>
2022-09-13 01:19:07 +0200

    Creating components

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="fiki">Dojo Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New blog</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
