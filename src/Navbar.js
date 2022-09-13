2e89424c66d1f4e137b6ab86e827699e4b71e76c
Filip Popovski <popovski_f@hotmail.com>
2022-09-13 16:42:11 +0200

    styling components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,13 @@
 const Navbar = () => {
   return (
     <div>
-      <h1 className="fiki">Dojo Blog</h1>
-      <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New blog</a>
-      </div>
+      <nav className="navbar">
+        <h1>Dojo Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create">New blog</a>
+        </div>
+      </nav>
     </div>
   );
 };
