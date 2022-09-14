4e6e5121ccdfdcb0352dbcc662e59e54d5d1e8a5
Filip Popovski <popovski_f@hotmail.com>
2022-09-14 19:46:29 +0200

    Mapping through blogs

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,36 @@
 import { useState } from "react";
 
 const Home = () => {
-  //let name = "Filip";
-  const [name, setName] = useState("Luigi");
-  const [age, setAge] = useState(19);
-
-  const handleClick = () => {
-    setName("Filip");
-    setAge(25);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "Et deserunt ex nulla veniam anim ad Lorem amet excepteur in aute sit nisi. Est sit officia cillum deserunt quis aliquip excepteur commodo dolor cillum enim sint. Id dolor adipisicing mollit proident duis.",
+      author: "Filip",
+      id: 1,
+    },
+    {
+      title: "Welcome party",
+      body: "Nisi veniam reprehenderit aute in excepteur consequat magna. Nostrud nisi do sunt aute cillum labore aliqua reprehenderit magna laboris amet irure. Nostrud reprehenderit irure qui ex ad proident.",
+      author: "Mario",
+      id: 2,
+    },
+    {
+      title: "Web dev tools",
+      body: "Est commodo irure dolore voluptate ullamco culpa pariatur. Dolore ullamco eiusmod in do sunt labore qui anim officia voluptate duis. Labore sint cillum ad non fugiat mollit occaecat ea commodo. In eiusmod tempor proident amet cillum dolor commodo sunt laborum reprehenderit laborum. Occaecat excepteur ut voluptate irure ad. Nostrud exercitation duis laboris aliqua consectetur minim et. Cupidatat elit fugiat fugiat id aliquip.",
+      author: "Luigi",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>HomePage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.body}</p>
+          <h3>Written by {blog.author}</h3>
+        </div>
+      ))}
     </div>
   );
 };
