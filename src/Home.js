e1399a25dca787c9849fcbb54c21ebc0eefea4b4
Filip Popovski <popovski_f@hotmail.com>
2022-09-14 16:09:25 +0200

    Click and things

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Alo brej", e);
+  };
+
+  const handleClick2 = (name, e) => {
+    console.log("Hello " + name, e.target);
+  };
   return (
     <div className="home">
       <h2>HomePage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClick2("Filip", e)}>Click me again</button>
     </div>
   );
 };
