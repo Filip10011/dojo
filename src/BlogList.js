435caa169c15ec7c2f91880d6ecd34a9ab06c574
Filip Popovski <popovski_f@hotmail.com>
2022-09-18 19:26:24 +0200

    Using useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -5,8 +5,7 @@ const BlogList = ({ blogs, title, handleDelete }) => {
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
-          <p>{blog.body}</p>
-          <h3>Written by {blog.author}</h3>
+          <p>Written by {blog.author}</p>
           <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
