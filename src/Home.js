6fcea709523ca835c1e20c9ddd324ece5b68552a
Filip Popovski <popovski_f@hotmail.com>
2022-09-19 16:43:52 +0200

    Fetching errors

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,38 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState("Filip");
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err.message);
+        });
+    }, 800);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isLoading && (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      )}
       {blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
   );
