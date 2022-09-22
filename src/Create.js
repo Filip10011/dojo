eae738fc21ce6421c85cd1cd0d4b8a434b4e0810
Filip Popovski <popovski_f@hotmail.com>
2022-09-22 15:01:06 +0200

    submit form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
   return (
     <div className="create">
       <h2>Add a new blog!</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Filip">Filip</option>
+          <option value="Andrej">Andrej</option>
+        </select>
+        <button>Add blog</button>
+      </form>
     </div>
   );
 };
